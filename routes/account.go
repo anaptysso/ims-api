d1f41fcf975b4d864cbaa7eb6727c2d3a6c99c32
Md Muidul Alam <md.muidul.alam@gmail.com>
2020-05-19 23:00:51 +0600

    IMS-25: Implement auto generated api documentation

--- routes/account.go
+++ routes/account.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	echo "github.com/labstack/echo"
+	echo "github.com/labstack/echo/v4"
 
 	managers "imsapi/managers/abstractions"
 	accountViewModels "imsapi/viewModels/account"
@@ -19,16 +19,17 @@ func (this Account) New() {
 // @Summary Signup an account test
 // @Description It will register new users if the post data provided properly.
 // @ID signup-new-account
-// @Router /account/signup [post]
 // @Accept  json
 // @Param fullName body string true "FullName"
 // @Param email 	body string true "Email"
 // @Param password 	body string true "Password"
+// @Param activationCode body string true "ActivationCode"
 // @Success  202	"Activated new account and updated necessary data."
 // @Failure  401	"The activation code didn't matched."
 // @Failure  406	"The account already activated."
 // @Failure  404	"The account is not created yet by administrator."
 // @Failure  400	"The server cannot or will not process the request due to an apparent client error."
+// @Router /account/signup [post]
 func (this Account) SignUp(c echo.Context) (err error) {
 	signUpViewModel := new(accountViewModels.SignUpViewModel)
 
